7c108b09c6fdd1055bc95a6060277352262c9ba5
junioir <junior.tefo01@gmail.com>
2025-05-07 03:32:18 -0400

    afficher

--- ProjetSynthese/Assets/Projet Synthèse Scène/Script2/Script2/ShopButton.cs
+++ ProjetSynthese/Assets/Projet Synthèse Scène/Script2/Script2/ShopButton.cs
@@ -11,23 +11,41 @@ public class ShopButton : MonoBehaviour
     {
         button = GetComponent<Button>();
 
-      
-        ResourceManager.Instance.OnResourceChanged += CheckResources;
+       
+        button.onClick.AddListener(AttemptPurchase);
 
         
+        ResourceManager.Instance.OnResourceChanged += CheckResources;
+
         CheckResources();
     }
 
     private void OnDestroy()
     {
-       
         if (ResourceManager.Instance != null)
             ResourceManager.Instance.OnResourceChanged -= CheckResources;
+
+        button.onClick.RemoveListener(AttemptPurchase);
     }
 
     private void CheckResources()
     {
         int current = ResourceManager.Instance.GetCurrentResource();
         button.interactable = current >= itemCost;
+    }
+
+    private void AttemptPurchase()
+    {
+        bool success = ResourceManager.Instance.SpendResource(itemCost);
+
+        if (success)
+        {
+            Debug.Log("Achat réussi pour " + itemCost + " ressources !");
+            
+        }
+        else
+        {
+            Debug.Log("Pas assez de ressources !");
+        }
     }
 }
